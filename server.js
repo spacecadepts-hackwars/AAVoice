b30bdf83fa0aca67c35293329ac42d1bc08654a8
844452 <harrison.bornstein@aa.com>
2017-05-19 21:41:21 -0500

    WIP

--- server.js
+++ server.js
@@ -45,6 +45,47 @@ app.get("/flights",function(request, response){
 });
 
 
+// console.log('hook request');
+
+//     try {
+//         var speech = 'empty speech';
+
+//         if (req.body) {
+//             var requestBody = req.body;
+
+//             if (requestBody.result) {
+//                 speech = '';
+
+//                 if (requestBody.result.fulfillment) {
+//                     speech += requestBody.result.fulfillment.speech;
+//                     speech += ' ';
+//                 }
+
+//                 if (requestBody.result.action) {
+//                     speech += 'action: ' + requestBody.result.action;
+//                 }
+//             }
+//         }
+
+//         console.log('result: ', speech);
+
+//         return res.json({
+//             speech: speech,
+//             displayText: speech,
+//             source: 'apiai-webhook-sample'
+//         });
+//     } catch (err) {
+//         console.error("Can't process request", err);
+
+//         return res.status(400).json({
+//             status: {
+//                 code: 400,
+//                 errorType: err.message
+//             }
+//         });
+//     }
+
+
 
 // app.use(bodyParser.urlencoded());
 // app.use(bodyParser.json());
