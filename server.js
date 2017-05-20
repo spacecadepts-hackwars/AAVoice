a709f6c119c0fb6bd225acd586c1b35b852c1462
844452 <harrison.bornstein@aa.com>
2017-05-19 21:44:19 -0500

    WIP

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express'),
     router = require('express').Router();
 var bodyParser = require('body-parser'); 
 var mastercard = require('./lib/controllers/mastercard.js');
+api = require('./lib/controllers/api.js');
 
 /**
  * Main application file
@@ -23,6 +24,9 @@ require('./lib/config/express')(app);
 
 app.listen(config.port, function () {
   console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+  api.get(getFlights, function (error,result){
+    console.log(result);
+	});
   // mastercard.sendPayment(amount,paymentDescription,expiraryMonth,expiraryYear,cardCVC,numb);
 });
 
@@ -89,6 +93,17 @@ app.get("/flights",function(request, response){
 
 // app.use(bodyParser.urlencoded());
 // app.use(bodyParser.json());
+
+
+var getFlights = { 
+    method: 'GET',
+    url: 'https://api.test.sabre.com/v1/shop/flights?origin=JFK&destination=LAX&departuredate=2017-07-07&returndate=2017-07-10&excludedcarriers=US%2CDL&outboundflightstops=0&inboundflightstops=0&onlineitinerariesonly=N&limit=3&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc&pointofsalecountry=US',
+    headers: 
+    { 
+        'Authorization':'Bearer T1RLAQIwc1AJ5GH/P2QQfnkGuF0RggIG2hAgdOCGfOe1a2ssQIfPJtY2AADAIDinsa9Revgw8tgP68fcjXmIX4BHmjQxj1Bs0LmK+hYrZRHfBQfuFamusocOfyL+5BtjYSEV/98+BwLRItpem8qZmkn/YQKNEyidTrp1o3RI3f3ZJ4sLVjCja9b2jo1O+VdUW6pgNVRMiJkvao4XmHib5tHbPHjBqs+smXc7sJ1cEBiZr4uMtQItJmeiElkXtaj9R+zw7t3qRNtEMtZvDnzh9nQMtWqzsgpd+miN9+dlKB4JF4E+OwYIoGV+3Ovn',
+        'X-Originating-Ip':12.39.178.119 
+    } };
+
 
 
 // Expose app
